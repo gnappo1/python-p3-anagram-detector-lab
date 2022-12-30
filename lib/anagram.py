ba3d26886d8dbea90704ecc41dd583389356fa21
Matteo Piccini <gnappo.1@live.it>
2022-12-30 00:49:41 -0800

    Done with set

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,6 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, matches):
+        return [match for match in matches if set(match) == set(self.word)]
